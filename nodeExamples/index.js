02d93b3b9b3ae62cc14ac8f0ed070db22707cb88
Richard Gordon <richgordon95@gmail.com>
2018-04-08 18:25:14 -0400

    replace rect var with imported module

--- nodeExamples/index.js
+++ nodeExamples/index.js
@@ -1,9 +1,5 @@
 var rect = require('./rectangle');
 
-var rect = {
-	perimeter: (x, y) => (2*(x+y)),
-	area: (x, y) => (x*y)
-};
 
 function solveRect(l,b) {
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
